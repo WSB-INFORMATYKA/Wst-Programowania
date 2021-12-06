cb0f2ce1397984623340b12b97e290fe3ce89331
KrzyStaChow <59803454+KrzyStaCho@users.noreply.github.com>
2021-12-06 14:24:35 +0100

    Utworzenie przykładu 7

---
+++ Materiały/Szkolne/Notatki/Przykłady/przyklad7.cpp
@@ -0,0 +1,15 @@
+// Author: Krzysztof Chowaniec
+// Date: 06.12.2021
+
+#include <iostream> //Biblioteka podstawowa
+
+//Serce programu
+int main()
+{
+	//Komentarz 1 linijkowy, w nastepnej linijce przestaje dzialac
+	std::cout << "Jestem poza komentarzem, wykonam sie\n";
+
+	/*Komentarz wieloliniowy, poki nie dasz konca komentarza wszystko zostanie zakomentowane
+	std::cout << "Jestem w komentarzu, nie wykonam sie\n";
+	*/
+}
