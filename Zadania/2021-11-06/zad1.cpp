088cc52d7f87aef751c7c4070d13b24d5826c401
KrzyStaChow <59803454+KrzyStaCho@users.noreply.github.com>
2021-12-18 14:44:21 +0100

    Dodanie zadania
    Zadanie pierwsze:
    Program pobiera dwa dane i wykonuje na nich obliczenia

--- Zadania/2021-11-06/zad1.cpp
+++ Zadania/2021-11-06/zad1.cpp
@@ -1,1 +1,25 @@
+// Author: Krzysztof Chowaniec
+// Date: 18.12.2021
+// Zadanie nr. 1
 
+//Napisane na: Visual Studio 2019
+//Testowane na: www.replit.com
+
+#include <iostream>
+
+using namespace std;
+
+//Program pobiera dwa dane i wykonuje na nich obliczenia
+int main()
+{
+    int x, y, wynik;
+
+    cout << "Podaj liczbe x: "; cin >> x;
+    cout << "podaj liczbe y: "; cin >> y;
+
+    wynik = x - y;
+    cout << "Wynik x - y = " << wynik << "\n";
+
+    wynik *= 2;
+    cout << "Wynik * 2 = " << wynik << " oraz wiekszy o 5 = " << wynik + 5 << "\n";
+}
